45ae3613728a5379645d71a72f1dde5570d681ee
jagdish <jcmishra1@gmail.com>
2019-07-04 15:45:10 +0530

    database changed to songLyrics

--- lyrics/app.js
+++ lyrics/app.js
@@ -14,7 +14,7 @@ const express = require('express'),
        flash=require("connect-flash")
        seedDB=require("./seeds");
 
-       var uri="mongodb+srv://jag:Aceandjd1@@cluster0-q8th7.mongodb.net/test?retryWrites=true&w=majority/songLyrics"
+       var uri="mongodb+srv://jag:Aceandjd1@@cluster0-q8th7.mongodb.net/songLyrics"
        mongoose.connect(uri,{useNewUrlParser:true});
        
 
